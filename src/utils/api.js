be17206b417a717f804ca0501013c8760bafa736
Ali Hassan <alihassan.octacer@gmail.com>
2025-11-29 11:07:43 +0500

    updates

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://13.210.143.91:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://mnworks.site/api';
 
 const api = axios.create({
   baseURL: API_URL,
